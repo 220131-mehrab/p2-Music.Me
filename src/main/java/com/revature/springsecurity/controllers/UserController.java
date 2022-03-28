c4e504c86b06b30d69141b90124c1ac1bdb657b6
DarrylWitcher <Breach097@gmail.com>
2022-03-28 08:35:56 -0700

    This doesn't work, uploading so others can take a look

---
+++ src/main/java/com/revature/springsecurity/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.revature.springsecurity.controllers;
+import com.revature.springsecurity.UserData;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController{
+    @GetMapping("/users")
+    public List<UserData> getUsers() {
+        List<UserData> users = new ArrayList<>();
+        users.add(new UserData(1,"Darryl", "darrylbraijon152@revature.net", "#1love2C0de"));
+        return users;
+    }
+}
