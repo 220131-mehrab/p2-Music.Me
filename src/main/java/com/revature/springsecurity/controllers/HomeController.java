d471641f68deaa3c056caa226d4c04c55767c59c
xxxjahlilallenxxx <jahlilallen@gmail.com>
2022-03-31 14:37:18 -0400

    Updated application.properties & Updated HomeCtrlr

--- src/main/java/com/revature/springsecurity/controllers/HomeController.java
+++ src/main/java/com/revature/springsecurity/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.revature.springsecurity.controllers;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Component
 @RestController
 @RequestMapping("/")
 public class HomeController {
